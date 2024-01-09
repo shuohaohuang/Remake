f64ad0c9371c9982b780e787d2a33c408edfb6f5
shuohaohuang <shuohao.huang.7e7@itb.cat>
2024-01-09 06:07:57 +0100

    new library "Utilities"

---
+++ Utilities/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace Utilities
+{
+    public class Utility
+    {
+        public static bool InRange(int num, int max)
+        {
+            return  num <= max;
+        }
+        public static bool InRange(float num, float min, float max)
+        {
+            return (num >= min && num <= max);
+        }
+    }
+}
